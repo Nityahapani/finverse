4637587b5642d7b7630b1958c5b99599b8f2cbc3
Nityahapani <118457303+Nityahapani@users.noreply.github.com>
2026-03-23 14:20:55 +0530

    Update __init__.py

--- finverse/models/__init__.py
+++ finverse/models/__init__.py
@@ -4,12 +4,16 @@ from finverse.models.three_statement import ThreeStatement, ThreeStatementAssump
 from finverse.models.comps import analyze as comps
 from finverse.models.ddm import gordon, h_model, multistage
 from finverse.models.sotp import Segment, analyze as sotp
+from finverse.models.regime_dcf import analyze as regime_dcf
+from finverse.models.synthetic_peers import build_peers as synthetic_peers
+from finverse.models.options import call as option_call, put as option_put, implied_vol, vol_surface
+from finverse.models.bonds import price as bond_price, ytm_from_price, price_yield_table
 from finverse.models import macro
 
 __all__ = [
-    "DCF", "DCFAssumptions", "DCFResults",
-    "LBO", "LBOAssumptions", "LBOResults",
-    "ThreeStatement", "ThreeStatementAssumptions",
-    "comps", "gordon", "h_model", "multistage",
-    "Segment", "sotp", "macro",
+    "DCF", "LBO", "ThreeStatement", "comps", "gordon", "h_model", "multistage",
+    "Segment", "sotp", "regime_dcf", "synthetic_peers",
+    "option_call", "option_put", "implied_vol", "vol_surface",
+    "bond_price", "ytm_from_price", "price_yield_table",
+    "macro",
 ]
